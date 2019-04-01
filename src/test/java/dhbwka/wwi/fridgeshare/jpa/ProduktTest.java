652f16494646fce6daa3da8ed4537fb1475a8f52
Max Wittenberger <maxwittenberger@gmail.com>
2019-04-01 21:00:14 +0200

    Mehr Tests

--- src/test/java/dhbwka/wwi/fridgeshare/jpa/ProduktTest.java
+++ src/test/java/dhbwka/wwi/fridgeshare/jpa/ProduktTest.java
@@ -9,13 +9,29 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
  * @author maxwi
  */
 public class ProduktTest {
+    Produkt produkt = new Produkt("Apfel", "3" , dhbwka.wwi.fridgeshare.jpa.ProduktKategorie.Obst, ProduktMaßeinheit.ml, "K", "max");
+    
     @Test
     public void testGetOwner() {
         System.out.println("getOwner");
-        Produkt produkt = new Produkt("Apfel", "3" , dhbwka.wwi.fridgeshare.jpa.ProduktKategorie.Obst, ProduktMaßeinheit.ml, "K", "max");
         String expResult = "max";
         String result = produkt.getOwner() ;
         assertEquals(expResult, result);
     }
     
+    @Test 
+    public void testgetName(){
+        System.out.println("getName");
+        String expResult = "Apfel";
+        String result = produkt.getName();
+        assertEquals(expResult, result);
+    }
+    
+     @Test 
+    public void testgetOrt(){
+        System.out.println("getOrt");
+        String expResult = "K";
+        String result = produkt.getOrt();
+        assertEquals(expResult, result);
+    }
 }
